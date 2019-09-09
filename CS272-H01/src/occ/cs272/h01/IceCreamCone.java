66c72a5f6a31dd688149c46d534fe36c489e71f1
Melissa Huynh <Melissa Huynh@192.168.11.5>
2019-09-09 00:48:09 -0700

    icecreamcone finish

--- CS272-H01/src/occ/cs272/h01/IceCreamCone.java
+++ CS272-H01/src/occ/cs272/h01/IceCreamCone.java
@@ -12,12 +12,33 @@ package occ.cs272.h01;
 public class IceCreamCone
 {
     // TODO: You must fill these in correctly to get credit
-    public static final String STUDENT = "Put your login ID here";
+    public static final String STUDENT = "mhuynh76";
     public static final String ASSIGNMENT = "H01-B";
 
     // TODO: Complete this class
     
+    double height;
+    double radius;
+    double length; 
+    public IceCreamCone(double height, double radius)
+    {
+    	 this.height = height; 
+    	 this.radius = radius;
+    }
     
+    public double getSurfaceArea()
+    {
+    	length = Math.sqrt(Math.pow(radius,2.0) 
+    			+ Math.pow(height, 2.0));
+    	
+    	return Math.PI * this.radius 
+    			* (this.radius + length);
+    }
     
-    
+    public double getVolume()
+    {
+    	return (1.0/3.0) * Math.PI 
+    			* Math.pow(radius, 2.0) 
+    			* this.height;
+    }
 }
